7765116b577049c1fde66420e475cef46dd5e7b3
mgmkei <rstoyo3.mizugami.keisuke@techcamp-training.com>
2024-06-04 18:12:24 +0900

    migrateとバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 6 }
+  validates :nickname,        presence: true
+  validates :first_name,      presence: true
+  validates :last_name,       presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana,  presence: true
+  validates :birth_date,      presence: true
 end
