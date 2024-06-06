fb35e1fe8b83a88c1471af1712d480402f5762c4
mgmkei <rstoyo3.mizugami.keisuke@techcamp-training.com>
2024-06-06 11:26:08 +0900

    名前とカナ名前のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  ZENKAKU_REGEXP = /\A[ぁ-んァ-ン一-龥]/
+  KATAKANA_REGEXP = /\A[ァ-ヶー－]+\z/
+
   validates :password, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 6 }
   validates :nickname,        presence: true
-  validates :first_name,      presence: true
-  validates :last_name,       presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana,  presence: true
+  validates :first_name,      presence: true, format: { with: ZENKAKU_REGEXP }
+  validates :last_name,       presence: true, format: { with: ZENKAKU_REGEXP }
+  validates :first_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
+  validates :last_name_kana,  presence: true, format: { with: KATAKANA_REGEXP }
   validates :birth_date,      presence: true
 end
